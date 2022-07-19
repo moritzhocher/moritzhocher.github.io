55f7c9d2bb65a6c6e3b245edfae8d20c45231477
moritzhocher <moritz.hocher@googlemail.com>
2022-07-19 03:44:33 +0200

    fullscreen on traditional art page working

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,11 +2,10 @@
 // popup-image
 
 function Fullview(imgsrc){
-  alert(imgsrc);
-  document.getElementById('overlayimg').src = imgsrc;
-  document.getElementById('overlay').style.display = 'block';
+  document.getElementById('FullscreenImg').src = imgsrc;
+  document.getElementById('FullscreenPopup').style.display = 'block';
 }
 
-function CloseFullView(){
-  document.getElementById('overlay').style.display = 'none';
+function CloseFullscreen(){
+  document.getElementById('FullscreenPopup').style.display = 'none';
 }
