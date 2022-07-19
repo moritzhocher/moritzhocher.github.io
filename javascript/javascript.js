67a44da34be71379cfb2e1feb7bb99689eb5a03d
moritzhocher <moritz.hocher@googlemail.com>
2022-07-19 03:14:31 +0200

    fullscreen img not working yet

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,12 +1,12 @@
-var video = document.getElementById("myVideo");
-var btn = document.getElementById("myBtn");
 
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
+// popup-image
+
+function Fullview(imgsrc){
+  alert(imgsrc);
+  document.getElementById('overlayimg').src = imgsrc;
+  document.getElementById('overlay').style.display = 'block';
+}
+
+function CloseFullView(){
+  document.getElementById('overlay').style.display = 'none';
 }
